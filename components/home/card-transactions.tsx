16f3028bbdc3e1d5b9866a21bcd8c405b018e84e
Ambro-Dev <piotr.ambroziak@ambro.dev>
2023-08-22 12:22:07 +0200

    data update

--- components/home/card-transactions.tsx
+++ components/home/card-transactions.tsx
@@ -1,150 +1,150 @@
-import {Avatar, Card, Text} from '@nextui-org/react';
-import React from 'react';
-import {Box} from '../styles/box';
-import {Flex} from '../styles/flex';
+import { Avatar, Card, Text } from "@nextui-org/react";
+import React from "react";
+import { Box } from "../styles/box";
+import { Flex } from "../styles/flex";
 
 export const CardTransactions = () => {
-   return (
-      <Card
-         css={{
-            mw: '375px',
-            height: 'auto',
-            bg: '$accents0',
-            borderRadius: '$xl',
-            // alignContent: 'start',
-            justifyContent: 'start',
-            px: '$6',
-         }}
-      >
-         <Card.Body css={{py: '$10'}}>
-            <Flex css={{gap: '$5'}} justify={'center'}>
-               <Text h3 css={{textAlign: 'center'}}>
-                  Latest Transactions
-               </Text>
-            </Flex>
-            <Flex
-               css={{gap: '$6', py: '$4'}}
-               // align={'center'}
+  return (
+    <Card
+      css={{
+        mw: "375px",
+        height: "auto",
+        bg: "$accents0",
+        borderRadius: "$xl",
+        // alignContent: 'start',
+        justifyContent: "start",
+        px: "$6",
+      }}
+    >
+      <Card.Body css={{ py: "$10" }}>
+        <Flex css={{ gap: "$5" }} justify={"center"}>
+          <Text h3 css={{ textAlign: "center" }}>
+            Ostatnie transakcje
+          </Text>
+        </Flex>
+        <Flex
+          css={{ gap: "$6", py: "$4" }}
+          // align={'center'}
 
-               direction={'column'}
-            >
-               <Flex css={{gap: '$6'}} align={'center'} justify="between">
-                  <Avatar
-                     size="lg"
-                     pointer
-                     src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
-                     bordered
-                     color="gradient"
-                     stacked
-                  />
-                  <Text span size={'$base'} weight={'semibold'}>
-                     Jose Perez
-                  </Text>
-                  <Text span css={{color: '$green600'}} size={'$xs'}>
-                     4500 USD
-                  </Text>
-                  <Text span css={{color: '$accents8'}} size={'$xs'}>
-                     9/20/2021
-                  </Text>
-               </Flex>
+          direction={"column"}
+        >
+          <Flex css={{ gap: "$6" }} align={"center"} justify="between">
+            <Avatar
+              size="lg"
+              pointer
+              src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
+              bordered
+              color="gradient"
+              stacked
+            />
+            <Text span size={"$base"} weight={"semibold"}>
+              Adam Strzała
+            </Text>
+            <Text span css={{ color: "$green600" }} size={"$xs"}>
+              4500 PLN
+            </Text>
+            <Text span css={{ color: "$accents8" }} size={"$xs"}>
+              9.09.2023
+            </Text>
+          </Flex>
 
-               <Flex css={{gap: '$6'}} align={'center'} justify="between">
-                  <Avatar
-                     size="lg"
-                     pointer
-                     src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
-                     bordered
-                     color="gradient"
-                     stacked
-                  />
-                  <Text span size={'$base'} weight={'semibold'}>
-                     Andrew Steven
-                  </Text>
-                  <Text span css={{color: '$green600'}} size={'$xs'}>
-                     4500 USD
-                  </Text>
-                  <Text span css={{color: '$accents8'}} size={'$xs'}>
-                     9/20/2021
-                  </Text>
-               </Flex>
-               <Flex css={{gap: '$6'}} align={'center'} justify="between">
-                  <Avatar
-                     size="lg"
-                     pointer
-                     src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
-                     bordered
-                     color="gradient"
-                     stacked
-                  />
-                  <Text span size={'$base'} weight={'semibold'}>
-                     Ruben Garcia
-                  </Text>
-                  <Text span css={{color: '$green600'}} size={'$xs'}>
-                     1500 USD
-                  </Text>
-                  <Text span css={{color: '$accents8'}} size={'$xs'}>
-                     2/20/2022
-                  </Text>
-               </Flex>
-               <Flex css={{gap: '$6'}} align={'center'} justify="between">
-                  <Avatar
-                     size="lg"
-                     pointer
-                     src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
-                     bordered
-                     color="gradient"
-                     stacked
-                  />
-                  <Text span size={'$base'} weight={'semibold'}>
-                     Perla Garcia
-                  </Text>
-                  <Text span css={{color: '$green600'}} size={'$xs'}>
-                     200 USD
-                  </Text>
-                  <Text span css={{color: '$accents8'}} size={'$xs'}>
-                     3/20/2022
-                  </Text>
-               </Flex>
-               <Flex css={{gap: '$6'}} align={'center'} justify="between">
-                  <Avatar
-                     size="lg"
-                     pointer
-                     src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
-                     bordered
-                     color="gradient"
-                     stacked
-                  />
-                  <Text span size={'$base'} weight={'semibold'}>
-                     Mathew Funez
-                  </Text>
-                  <Text span css={{color: '$green600'}} size={'$xs'}>
-                     2444 USD
-                  </Text>
-                  <Text span css={{color: '$accents8'}} size={'$xs'}>
-                     5/20/2022
-                  </Text>
-               </Flex>
-               <Flex css={{gap: '$6'}} align={'center'} justify="between">
-                  <Avatar
-                     size="lg"
-                     pointer
-                     src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
-                     bordered
-                     color="gradient"
-                     stacked
-                  />
-                  <Text span size={'$base'} weight={'semibold'}>
-                     Carlos Diaz
-                  </Text>
-                  <Text span css={{color: '$green600'}} size={'$xs'}>
-                     3000 USD
-                  </Text>
-                  <Text span css={{color: '$accents8'}} size={'$xs'}>
-                     12/20/2022
-                  </Text>
-               </Flex>
-            </Flex>
-         </Card.Body>
-      </Card>
-   );
+          <Flex css={{ gap: "$6" }} align={"center"} justify="between">
+            <Avatar
+              size="lg"
+              pointer
+              src="https://i.pravatar.cc/150?u=a048581f4e29026701d"
+              bordered
+              color="gradient"
+              stacked
+            />
+            <Text span size={"$base"} weight={"semibold"}>
+              Adrzej Kowalski
+            </Text>
+            <Text span css={{ color: "$green600" }} size={"$xs"}>
+              3500 PLN
+            </Text>
+            <Text span css={{ color: "$accents8" }} size={"$xs"}>
+              31.12.2022
+            </Text>
+          </Flex>
+          <Flex css={{ gap: "$6" }} align={"center"} justify="between">
+            <Avatar
+              size="lg"
+              pointer
+              src="https://i.pravatar.cc/150?u=a092581d4ef9026700d"
+              bordered
+              color="gradient"
+              stacked
+            />
+            <Text span size={"$base"} weight={"semibold"}>
+              Róża Nowak
+            </Text>
+            <Text span css={{ color: "$green600" }} size={"$xs"}>
+              1500 PLN
+            </Text>
+            <Text span css={{ color: "$accents8" }} size={"$xs"}>
+              2.08.2023
+            </Text>
+          </Flex>
+          <Flex css={{ gap: "$6" }} align={"center"} justify="between">
+            <Avatar
+              size="lg"
+              pointer
+              src="https://i.pravatar.cc/150?u=a04258114e29026702d"
+              bordered
+              color="gradient"
+              stacked
+            />
+            <Text span size={"$base"} weight={"semibold"}>
+              Paweł Tarnowski
+            </Text>
+            <Text span css={{ color: "$green600" }} size={"$xs"}>
+              200 PLN
+            </Text>
+            <Text span css={{ color: "$accents8" }} size={"$xs"}>
+              12.08.2023
+            </Text>
+          </Flex>
+          <Flex css={{ gap: "$6" }} align={"center"} justify="between">
+            <Avatar
+              size="lg"
+              pointer
+              src="https://i.pravatar.cc/150?u=a048581f4e29026701d"
+              bordered
+              color="gradient"
+              stacked
+            />
+            <Text span size={"$base"} weight={"semibold"}>
+              Mateusz Górski
+            </Text>
+            <Text span css={{ color: "$green600" }} size={"$xs"}>
+              2444 PLN
+            </Text>
+            <Text span css={{ color: "$accents8" }} size={"$xs"}>
+              5.08.2023
+            </Text>
+          </Flex>
+          <Flex css={{ gap: "$6" }} align={"center"} justify="between">
+            <Avatar
+              size="lg"
+              pointer
+              src="https://i.pravatar.cc/150?u=a04258114e29026702d"
+              bordered
+              color="gradient"
+              stacked
+            />
+            <Text span size={"$base"} weight={"semibold"}>
+              Karolina Adamczyk
+            </Text>
+            <Text span css={{ color: "$green600" }} size={"$xs"}>
+              3000 PLN
+            </Text>
+            <Text span css={{ color: "$accents8" }} size={"$xs"}>
+              12.08.2023
+            </Text>
+          </Flex>
+        </Flex>
+      </Card.Body>
+    </Card>
+  );
 };
